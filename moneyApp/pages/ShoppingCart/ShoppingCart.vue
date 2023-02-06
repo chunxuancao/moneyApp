c3ae61829d1b32bdb6a6e9a1a4ba339b8d2a452a
cao18 <cao18373503328@outlook.com>
2023-02-06 22:16:36 +0800

    app的四个页面（未完结）

---
+++ moneyApp/pages/ShoppingCart/ShoppingCart.vue
@@ -0,0 +1,39 @@
+<template>
+	<view class="top">购物车 地址             编辑
+	 <view class="picSize">
+	 	<img src="static/更多.png" alt="快捷入口">
+	 </view>
+	
+		<view class="sellBackground">
+			<view class="sellBox">
+				物品陈列
+			</view>
+		</view>
+		<view class="bottom">
+			底部
+		</view>
+	</view>
+
+
+</template>
+<script>
+	
+</script>
+<style>
+	.top{
+		
+	},
+	.sellBackground{
+		
+	},
+	.sellBox{
+		
+	},
+	.bottom{
+		
+	}
+	.picSize{
+		width: 1px;
+		height: 1px;
+	}
+</style>
